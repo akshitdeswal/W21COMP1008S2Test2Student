237cd83ef52f6679e9bd58a9ea8df90d4383a2f8
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-03-04 12:30:11 -0500

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.List;
+/*
+Name : Akshit Deswal
+Student : 200454399
+ */
+public class Car {
+    private String make, model;
+    private int mileage;
+    private double price;
+
+    public Car(String make, String model, int mileage, double price) {
+        setMake(make);
+        setModel(model);
+        setMileage(mileage);
+        setPrice(price);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setMake(String make) {
+    }
+
+    public void setModel(String model) {
+    }
+
+    public void setMileage(int mileage) {
+    }
+
+    public void setPrice(double price) {
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+}
